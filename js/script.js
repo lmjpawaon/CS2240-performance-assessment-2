341568a723b049549c4376604154c3220eedb6cc
lmjpawaon <lmjpawaon@addu.edu.ph>
2022-01-18 10:03:12 +0800

    update scripts - added bootstrap validation code (not working) - added script for image upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+(function () {
+  const forms = document.querySelectorAll('.needs-validation')
+  Array.from(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+  function showPreview(event){
+    if(event.target.files.length > 0){
+      var src = URL.createObjectURL(event.target.files[0]);
+      var preview = document.getElementById("imagePreview");
+      preview.src = src;
+      preview.style.display = "block";
+    }
+  }
