e96928c5fb45a71e27fad977cca5039b9faf8565
lmjpawaon <lmjpawaon@addu.edu.ph>
2022-01-26 00:07:37 +0800

    update script - removed script for form validation and moved it to the html file itself

--- js/script.js
+++ js/script.js
@@ -1,20 +1,4 @@
 
-  const forms = document.querySelectorAll (".needs-validation");
-
-  Array.prototype.slice.call (forms).forEach (function(form)
-  {
-    form.addEventListener ("submit", function(event)
-    {
-    if (!form.checkValidity())
-      {
-      event.preventDefault();
-      event.stopPropagation();
-      }
-      form.classList.add("was-validated");
-    });
-  });
-
-
 function showPreview(event){
   if(event.target.files.length > 0){
     var src = URL.createObjectURL(event.target.files[0]);
